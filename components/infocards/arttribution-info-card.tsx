08de20e0e45268f949884c2eb00c8a58c90fd621
bashybaranaba <bashybaranaba@gmail.com>
2024-06-06 01:10:56 +0300

    feat:add info cards

---
+++ components/infocards/arttribution-info-card.tsx
@@ -0,0 +1,17 @@
+import { Card, CardContent } from "@/components/ui/card";
+export default function ArttributionInfoCard() {
+  return (
+    <>
+      <Card className="lg:m-4 shadow-md p-4  mb-4">
+        <CardContent className="p-7">
+          <h2 className="text-2xl font-bold "> Automatic Attributions </h2>
+          <p className="text-base text-gray-600">
+            Arttribute is centerd around attributions, ensuring that artists get
+            proper credit when their work is used in AI by automatically
+            appending attribution details to AI-generated content.
+          </p>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
---
+++ components/infocards/licenses-info-card.tsx
@@ -0,0 +1,43 @@
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ChevronRight } from "lucide-react";
+
+export default function LicensesInfoCard() {
+  return (
+    <>
+      <Card className="lg:m-4 shadow-md border-purple-300 lg:p-4 rounded-xl">
+        <CardContent className="p-4">
+          <div className="lg:flex items-center justify-center">
+            <Image
+              src={"/licenses/png/arrtibute-exlusive-noncommercial-license.png"}
+              width={110}
+              height={90}
+              alt="Arttribute Licensing"
+              className="m-2 p-2"
+            />
+            <div className="m-2 p-2">
+              <h2 className="text-xl font-bold ">
+                Simple Code enforced licenses
+              </h2>
+              <p className="text-base text-gray-500">
+                Inspired by Creative Commons, Arttribute provides simple
+                code-enforceable licenses for granting AI usage permissions for
+                your work.
+              </p>
+            </div>
+          </div>
+          <Link href="/" passHref>
+            <Button variant="outline" className="m-2 mb-0 ">
+              <p className="text-sm font-medium bg-gradient-to-r from-pink-500 to-purple-500 bg-clip-text text-transparent">
+                Learn more
+              </p>
+              <ChevronRight className="h-4 w-4 text-purple-500" />
+            </Button>
+          </Link>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
